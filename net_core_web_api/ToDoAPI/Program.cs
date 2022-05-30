107271482d441300af08d74d11730f7c08eebd8f
Enmanuelle Acuna <emanuelacu@outlook.com>
2022-05-29 22:27:16 -0600

    Se ajusta Program.cs:
    No se usa MinimalAPI.
    Se inyecta contexto de base de datos generado a partir de la BD en PostgreSQL.

--- net_core_web_api/ToDoAPI/Program.cs
+++ net_core_web_api/ToDoAPI/Program.cs
@@ -1,30 +1,41 @@
+using Invoices.Models.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.AddDbContext<ToDoContext>(options => options.UseInMemoryDatabase("ToDoList"));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace ToDoAPI
 {
-	app.UseDeveloperExceptionPage();
-	// app.UseSwagger();
-	// app.UseSwaggerUI();
+	public class programm
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+
+			builder.Services.AddControllers();
+			builder.Services.AddDbContext<ToDoContext>(options => options.UseInMemoryDatabase("ToDoList"));
+			builder.Services.AddDbContext<InvoicesDBContext>(options => options.UseNpgsql());
+			// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+			builder.Services.AddEndpointsApiExplorer();
+			// builder.Services.AddSwaggerGen();
+
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				// app.UseSwagger();
+				// app.UseSwaggerUI();
+			}
+
+			app.UseHttpsRedirection();
+
+			app.UseAuthorization();
+
+			app.MapControllers();
+
+			app.Run();
+		}
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
