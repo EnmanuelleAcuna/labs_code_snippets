3050a28814a9364e82ef18cafa36d613c7c91646
Enmanuelle Acuña <emanuelacu@outlook.com>
2022-06-22 17:43:26 -0600

    Se quita de la url de Invoices el API, ya que uniformemente se accede directamente al endpoint sin especificar en la URL api.

--- net_core_web_api/REST_API/Controllers/InvoicesController.cs
+++ net_core_web_api/REST_API/Controllers/InvoicesController.cs
@@ -10,7 +10,7 @@ using REST_API.Models.Invoices;
 
 namespace REST_API.Controllers
 {
-	[Route("api/[controller]")]
+	[Route("[controller]")]
 	[ApiController]
 	public class InvoicesController : ControllerBase
 	{
